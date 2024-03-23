ecb8101a7e5c713836e728fcd8959db5da323da7
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-23 18:33:35 +0200

    5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -74,7 +74,3 @@ class FileStorage:
         if obj:
             key = "{}.{}".format(type(obj).__name__, obj.id)
             del self.__objects[key]
-            
-    def close(self):
-        """ Calls reload() function. """        
-        self.reload()
