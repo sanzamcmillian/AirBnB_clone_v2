95a3aad7041ea9b23657a55d3543e8523576bcb8
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-24 02:25:15 +0200

    10

--- models/place.py
+++ models/place.py
@@ -8,6 +8,12 @@ from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 import shlex
 
+place_amenity = Table("place_amenity", Base.metadata,
+                      Column("place_id", String(60), ForeignKey("places.id"),
+                             primary_key=True, nullable=False),
+                      Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                             primary_key=True, nullable=False))
+
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
@@ -26,7 +32,8 @@ class Place(BaseModel, Base):
     if getenv("HBNB_TYPE_STORAGE") == "db":
         reviews = relationship("Review", cascade='all, delete, delete-orphan',
                                backref="place")
-        
+        amenities = relationship("Amenity", secondary=place_amenity, viewonly=False,
+                                 back_populates="Place_amenities")
     else:
         @property
         def reviews(self):
@@ -42,4 +49,10 @@ class Place(BaseModel, Base):
             for elem in lista:
                 if (elem.place_id == self.id):
                     result.append(elem)
-            return (result)        
+            return (result)
+        
+        @property
+        def amenities(self, obj=None):
+            """ Appends amenity ids to the attribues. """
+            if type(obj) is Amenity and obj.id not in self.amenity_ids:
+                self.amenity_ids.append(obj.id)
