07801f0121672adc2487fce308d94dae9831b408
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-24 02:08:56 +0200

    9

--- models/review.py
+++ models/review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
