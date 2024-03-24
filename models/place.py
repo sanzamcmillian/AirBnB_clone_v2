662a33f2d456becf3b4a2f19b950fd2c117b33eb
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-24 02:05:31 +0200

    9

--- models/place.py
+++ models/place.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel, Base
 from os import getenv
 import models
-from sqlalchemy.orm import relationships
+from sqlalchemy.orm import relationship
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -22,3 +22,24 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
+    
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        reviews = relationship("Review", cascade='all, delete, delete-orphan',
+                               backref="place")
+        
+    else:
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id. """
+            var = models.storage.all()
+            lista = []
+            result = []
+            for key in var:
+                review = key.replace('.', ' ')
+                review = shlex.split(review)
+                if (review[0] == 'Review'):
+                    lista.append(var[key])
+            for elem in lista:
+                if (elem.place_id == self.id):
+                    result.append(elem)
+            return (result)        
