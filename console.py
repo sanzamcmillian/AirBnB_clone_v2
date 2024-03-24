9e1fc7fb52175efb04aa902c184b3d7f8f344e64
sanzamcmillian <g18s2658@ru.ac.za>
2024-03-24 03:24:44 +0200

    3

--- console.py
+++ console.py
@@ -119,12 +119,11 @@ class HBNBCommand(cmd.Cmd):
             if not args:
                 raise SyntaxError()
             arg_list = args.split(" ")
-            class_name = arg_list[0]
             kw = {}
             for arg in arg_list[1:]:
                 arg_splited = arg.split("=")
                 arg_splited[1] = eval(arg_splited[1])
-                if type(arg_splited[1] is str:
+                if type(arg_splited[1]) is str:
                     arg_splited[1] = arg_splited[1].replace("_", " ").replace('"', '\\"')   
                 kw[arg_splited[0]] = arg_splited[1]
         except SyntaxError:
